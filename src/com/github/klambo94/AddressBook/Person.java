a2d2e044a48274a7d75547ba5f743803367823d0
Kendra Lamb <klambo94@gmail.com>
2015-03-14 21:58:12 -0600

    1 AddressBook-1
    Create a person class

--- src/com/github/klambo94/AddressBook/Person.java
+++ src/com/github/klambo94/AddressBook/Person.java
@@ -4,5 +4,88 @@ package com.github.klambo94.AddressBook;
  * Created by Kendra Lamb on 3/14/2015.
  */
 public class Person {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String state;
+    private String email;
+    private int phoneNumber;
 
+    public Person(String firstName, String middleName, String lastName, int phoneNumber, String street, String city, String state, String email) {
+        this.setFirstName(firstName);
+        this.setMiddleName(middleName);
+        this.setLastName(lastName);
+        this.setPhoneNumber(phoneNumber);
+        this.setStreet(street);
+        this.setCity(city);
+        this.setState(state);
+        this.setEmail(email);
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
