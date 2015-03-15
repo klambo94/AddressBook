bc1019791120588dd61c32ceba6fb99ba1542846
Kendra Lamb <klambo94@gmail.com>
2015-03-15 09:51:39 -0600

    Closes #3 AddressBook-3 - Add a person
    {description}

--- src/com/github/klambo94/AddressBook/AddresssBook.java
+++ src/com/github/klambo94/AddressBook/AddresssBook.java
@@ -1,6 +1,8 @@
 package com.github.klambo94.AddressBook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 /**
  * Created by Kendra Lamb on 3/14/2015.
@@ -8,5 +10,18 @@ import java.util.ArrayList;
 public class AddresssBook {
     ArrayList<Person> people = new ArrayList();
 
-    
+    Scanner scanner = new Scanner(System.in); //to get the user's input about a person
+
+    public String addPerson() {
+        System.out.println("What is the first name of the person?");
+        String userInput = scanner.nextLine();
+        if (userInput.length() <= 2) {
+            System.out.println("You must enter a vaild name, more than 2 letters. Please try again. ");
+            userInput = scanner.nextLine();
+        }
+
+    }
+
+
+
 }
