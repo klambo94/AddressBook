81a8188ea798296d1ae51f7e430a2a5272f44fd6
Kendra Lamb <klambo94@gmail.com>
2015-03-14 20:51:52 -0600

    Initial commit

---
+++ src/com/github/klambo94/AddressBook/Main.java
@@ -0,0 +1,13 @@
+package com.github.klambo94.AddressBook;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //create a person class- first middle last name, street- city state zip, phone number, email
+        //address book class- array of people objects with methods that will interact with array
+        //add, edit, delete, search, sort(alphbetically by any catagory from acsending to desending) methods.
+        //print method to  display contacts out
+        //program will be operated through main class
+    }
+}
---
+++ src/com/github/klambo94/AddressBook/Person.java
@@ -0,0 +1,8 @@
+package com.github.klambo94.AddressBook;
+
+/**
+ * Created by Kendra Lamb on 3/14/2015.
+ */
+public class Person {
+
+}
