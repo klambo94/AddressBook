dbe2cb7e89a93eb0b322df72598c8ca53201aede
Kendra Lamb <klambo94@gmail.com>
2015-03-17 23:59:10 -0600

    Closes #12 AddressBook-12 - Add the print method to the add person method
    {description}

--- src/com/github/klambo94/AddressBook/Main.java
+++ src/com/github/klambo94/AddressBook/Main.java
@@ -3,7 +3,10 @@ package com.github.klambo94.AddressBook;
 public class Main {
 
     public static void main(String[] args) {
-
+        AddressBook addressBook = new AddressBook();
+        System.out.println("Welcome to the Address Book! Please enter your contacts here");
+        addressBook.addPerson();
+        System.out.print(addressBook.toString());
 
     }
 }
